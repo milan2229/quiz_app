5b35690ef5fc73ca769953b2917687f1ff666664
milan2229 <milan_1899.22@icloud.com>
2021-06-26 23:09:12 +0900

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,80 @@
 const quiz = [
   {
-    question: 'ゲーム史上、最も売れたゲーム機はどれ？',
-    answers: [ 'スーパーファミコン', 'PlayStation 2', 'ニンテンドーDS', 'Xbox 360'],
-    correct: 'ニンテンドーDS'
+    question: 'サッカーW杯最多優勝国はどこの国でしょうか？',
+    answers: [
+      'ウルグアイ',
+      'イタリア',
+      'ドイツ',
+      'ブラジル'
+    ],
+    correct: 'ブラジル'
   }, {
-    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
-    answers: [ 'MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
-    correct: 'MOTHER2'
-  }, {
-    question: 'ファイナルファンタジーⅣの主人公の名前は？',
-    answers: [ 'フリオニール', 'クラウド', 'ティーダ', 'セシル'],
-    correct: 'セシル'
+    question: 'UEFAチャンピオンズリーグ最多優勝チームは？',
+    answers: [
+      'FCバルセロナ',
+      'ACミラン',
+      'レアル・マドリード',
+      'リヴァプール'
+    ],
+    correct: 'レアル・マドリード'
+  },{
+    question: 'Jリーグ最多優勝チームはどこでしょう？',
+    answers: [
+      '川崎フロンターレ',
+      '鹿島アントラーズ',
+      '浦和レッズ',
+      '東京ヴェルディ'
+    ],
+    correct: '鹿島アントラーズ'
   }
 ];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+const $button = document.getElementsByTagName('button');
+
+// クイズの問題文、選択肢を定義
+const setupQuiz = () =>{
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
+  const buttonLength = $button.length;
+
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+// 上で定義した関数は読み込んであげる
+setupQuiz();
+
+const clickHandler = (e) =>{
+  if(quiz[quizIndex].correct === e.target.textContent){
+    window.alert('正解');
+    score++;
+  }else{
+    window.alert('不正解!');
+  }
+
+  quizIndex++;
+  
+  if(quizIndex < quizLength){
+    // 問題数がまだあればこちらを実行
+    setupQuiz();
+  } else {
+    // 問題数がもうなければ実行
+    window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です。');
+  }
+};
+
+// ボタンクリックしたら正誤判定
+// eはイベントでtargetはクリックされたボタン
+let handlerIndex = 0;
+const buttonLength = $button.length;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
